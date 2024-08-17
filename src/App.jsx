8d0a20de8f068a7b32a72edc6b1f1e4a5e7aa157
Jayanth Gudimella <gjaynir0508@gmail.com>
2024-08-17 13:46:19 +0530

    Complete all tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
+import React, { useState } from "react";
+import Header from "./components/Header";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Shop from './pages/Shop';
-import Login from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Login from "./pages/Login";
 
 const App = () => {
-  // Task 3: Use `useState` to manage the username state and pass it to the shop page
+	// Task 3: Use `useState` to manage the username state and pass it to the shop page
+	const [name, setName] = useState();
 
-  return (
-    <div>
-      <div className="main">
-        <Router>
-          {/* Task 1: Ensure that the header component is imported and rendered here */}
-          <Routes>
-            {/* Task 2: Implement routing to navigate between different pages */}
-            {/* Task 3: Pass the username state as a prop to the shop page */}
-            {/* Task 3: Pass the setUser function to the login page */}
-          </Routes>
-        </Router>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="main">
+				<Router>
+					{/* Task 1: Ensure that the header component is imported and rendered here */}
+					<Header />
+					<Routes>
+						{/* Task 2: Implement routing to navigate between different pages */}
+						<Route path="/" element={<Home />} />
+						{/* Task 3: Pass the username state as a prop to the shop page */}
+						<Route
+							path="/shop"
+							element={<Shop userName={name} />}
+						/>
+						{/* Task 3: Pass the setUser function to the login page */}
+						<Route
+							path="/login"
+							element={<Login setUserName={setName} />}
+						/>
+					</Routes>
+				</Router>
+			</div>
+		</div>
+	);
 };
 
 export default App;
